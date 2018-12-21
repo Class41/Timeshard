33698d8d82c60326c79edc527deb9f4bd0a36b43
tvxr5 <tvxr5@mail.umsl.edu>
2018-12-21 00:17:01 -0600

    this is a commit message to check if form fields

---
+++ accountCreation.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+
+<?php     
+	$valid = true; 
+?>
+
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="./styles/style.css"/>
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+	<title>Timeshard - Account Creation/title>
+</head>
+
+<body>
+	<div>
+	</div>
+	<div>
+			<form>
+				<input class="formitem" type="text" name="username" placeholder="Username"/> <br />
+				<input class="formitem" type="password" name="password" placeholder="Password"/> <br />
+				<input class="formitem" type="text" name="firstName" placeholder="First Name"/><br/> 
+				<input class="formitem"	type="text" name="lastName" placeholder="Last Name"/><br/>			
+				<input class="fromitem" type="email" name="email" placeholder="Email"/><br/>
+				<input id="loginbutton" type="submit" value="Create Account"/> <br />
+			</form>
+			<hr/>
+		</div>
+	</div>
+	<div>
+
+	</div>
+</body>
+</html>
