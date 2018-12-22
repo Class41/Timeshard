d1077fef1b1a44df92b5bdcd3ad30e12b915c881
tvxr5 <tvxr5@mail.umsl.edu>
2018-12-21 22:50:59 -0600

    This is the First push on 12/21/2018 by tvxr5

--- pages/shared/accountcreate.php
+++ pages/shared/accountcreate.php
@@ -6,6 +6,14 @@
     <link rel="stylesheet" type="text/css" href="../../styles/general.css" />
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
     <title>Timeshard - Register</title>
+
+    <?php
+		if($_SERVER["REQUEST_METHOD"] == "POST")
+		{
+			$GLOBALS["posted"] = true;
+			require("./scripts/login.php");
+		}
+	?>
 </head>
 
 <body>
@@ -19,13 +27,14 @@
             <h1 class="textneutral">Register</h1>
         </div>
         <form id="register" class="container" method="POST" action="accountcreate.php">
-            <input class="formitem" type="text" name="username" placeholder="Username" /> <br />
-			<input class="formitem" type="password" name="password" placeholder="Password" /> <br />
-			<input class="formitem" type="password" name="password2" placeholder="Repeat Password" /> <br />
-            <input class="formitem" type="text" name="firstName" placeholder="First Name" /><br />
-            <input class="formitem" type="text" name="lastName" placeholder="Last Name" /><br />
-            <input class="formitem" type="email" name="email" placeholder="Email" /><br />
-            <input class="button buttongreen" type="submit" value="Create" > <br />
+            <input class="formitem" type="text" name="username" placeholder="Username" /> <br /> <?php if($GLOBALS["posted"] == true) { echo "value=\"" . $_POST["username"] . "\""; } ?>/> <br />
+			<input class="formitem" type="password" name="password" placeholder="Password" /> <br /><?php if($GLOBALS["posted"] == true) { echo "value=\"" . $_POST["password"] . "\""; } ?>/> <br />
+			<input class="formitem" type="password" name="password2" placeholder="Repeat Password" /> <br /><?php if($GLOBALS["posted"] == true) { echo "value=\"" . $_POST["repeatPassword"] . "\""; } ?>/> <br />
+            $salt = rand();<?php if($GLOBALS["posted"] == true) { echo "value=\"" . $_POST["$salt"] . "\""; } ?>/> <br /> 
+            <input class="formitem" type="text" name="firstName" placeholder="First Name" /><br /><?php if($GLOBALS["posted"] == true) { echo "value=\"" . $_POST["firstName"] . "\""; } ?>/> <br />
+            <input class="formitem" type="text" name="lastName" placeholder="Last Name" /><br /><?php if($GLOBALS["posted"] == true) { echo "value=\"" . $_POST["lastName"] . "\""; } ?>/> <br />
+            <input class="formitem" type="email" name="email" placeholder="Email" /><br /><?php if($GLOBALS["posted"] == true) { echo "value=\"" . $_POST["email"] . "\""; } ?>/> <br />
+            <input class="button buttongreen" type="submit" value="Create" /> <br /><?php INSERT  INTO timeshard.accounts (username, password, salt, userdata) VALUES ("username", "password", "$salt", (INSERT INTO timeshard.user (firstname, lastname, email) VALUES ("firstName", "lastName", "email") SELECT SCOPE_IDENTITY())); ?>
         </form>
     </div>
     </div>
